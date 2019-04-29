49f03fddb9d4f245d0620fe3486f366108a9db91
marklishman <mark.lishman@gmail.com>
2019-04-29 11:51:00 +0100

    TODOs

--- green/src/main/java/io/lishman/green/controller/user/UserController.java
+++ green/src/main/java/io/lishman/green/controller/user/UserController.java
@@ -68,14 +68,12 @@ class UserController {
         return ResponseEntity.ok(userResource);
     }
 
-    // TODO use Resource???
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@RequestBody final User user) {
         return userService.createUser(user);
     }
 
-    // TODO use Resource???
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public User updateUser(
